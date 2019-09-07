0461c1526581d73c70e57c1f3b5fda48c0022485
김바다 / Pada <bada013@naver.com>
2019-09-07 14:48:49 +0900

    feature. 웹팩 환경 구축 (#4)
    * merged
    
    * feature: path 추가
    
    * feature: 공용 웹팩 추가
    
    프로덕션과 개발 환경에서 중복으로 사용하는 환경을 모았다. 코드 중복 제거 했다.
    
    * feature: 웹팩 개발환경 추가
    
    웹팩 개발환경에서 사용하는 데브서버 와 소스맵 추가했다.
    
    * feature: 프로덕션환경 추가

---
+++ build/webpack.prod.ts
@@ -0,0 +1,8 @@
+import webpack from 'webpack';
+
+export const config: webpack.Configuration = {
+    mode: 'production',
+    // entry: {
+    //     vendor: ['react', 'react-dom', 'react-router-dom', 'antd'],
+    // },
+};

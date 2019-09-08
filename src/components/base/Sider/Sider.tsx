c882c2d89db82dc88a0030de9865ee885f45a9f9
0261 <bada013@naver.com>
2019-09-08 23:45:38 +0900

    feature: 베이스 컴포넌트 생성

---
+++ src/components/base/Sider/Sider.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { Layout, Menu, Icon } from 'antd';
+import { Link } from 'react-router-dom';
+
+const { Sider } = Layout;
+const { SubMenu } = Menu;
+
+interface Props {
+    matchUrl: string;
+}
+export const SiderFC: React.FunctionComponent<Props> = ({ matchUrl }) => {
+    const [collapsed, setCollapsed] = useState(false);
+    return (
+        <Sider collapsible collapsed={collapsed} onCollapse={collapsed => setCollapsed(collapsed)}>
+            <Link to='/'>
+                <div className='logo' />
+            </Link>
+            <Menu theme='dark' defaultSelectedKeys={['/']} selectedKeys={[matchUrl]} mode='inline'>
+                <Menu.Item key='/users'>
+                    <Link to='/users'>
+                        <Icon type='user' />
+                        <span>User</span>
+                    </Link>
+                </Menu.Item>
+                <Menu.Item key='/dashboards'>
+                    <Link to='/dashboards'>
+                        <Icon type='dashboard' />
+                        Dashboard
+                    </Link>
+                </Menu.Item>
+                <SubMenu
+                    key='sub1'
+                    title={
+                        <span>
+                            <Icon type='pie-chart' />
+                            <span>Metrics</span>
+                        </span>
+                    }
+                >
+                    <Menu.Item key='3'>
+                        <span>metric1</span>
+                    </Menu.Item>
+                    <Menu.Item key='4'>
+                        <span>metric2</span>
+                    </Menu.Item>
+                    <Menu.Item key='5'>
+                        <span>metric3</span>
+                    </Menu.Item>
+                </SubMenu>
+                <SubMenu
+                    key='sub2'
+                    title={
+                        <span>
+                            <Icon type='project' />
+                            <span>Monitors</span>
+                        </span>
+                    }
+                >
+                    <Menu.Item key='6'>monitor1</Menu.Item>
+                    <Menu.Item key='7'>monitor2</Menu.Item>
+                    <Menu.Item key='8'>monitor3</Menu.Item>
+                </SubMenu>
+            </Menu>
+        </Sider>
+    );
+};

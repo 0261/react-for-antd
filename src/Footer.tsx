e30ec6508e70807876fe8098b6e0ba6dd7b19ca4
0261 <bada013@naver.com>
2019-09-08 23:44:46 +0900

    fix: 안쓰는 파일 삭제
    폴더 변경

--- src/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Layout from 'antd/lib/layout';
-const { Footer } = Layout;
-
-export const FooterFC: React.FunctionComponent = () => {
-    return <Footer style={{ textAlign: 'center' }}>Footer Here</Footer>;
-};

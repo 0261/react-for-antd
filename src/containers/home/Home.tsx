bc9155f9d3b23a3d1a96ec2fc49c33d5e8cac064
0261 <bada013@naver.com>
2019-09-10 11:10:57 +0900

    feature: home 컨테이너 추가

---
+++ src/containers/home/Home.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import HomeComponent from '../../components/home/Home';
+
+class DataSourceContainers extends Component {
+    render() {
+        return <HomeComponent></HomeComponent>;
+    }
+}
+
+export default DataSourceContainers;

10e4f31a576b843baa806265ba4167298050729b
0261 <bada013@naver.com>
2019-09-10 13:50:15 +0900

    feature: 데이터소스 선택 기능 추가
    데이터소스를 누르면 상단에 선택된 데이터소스가 나오고 Localstorage에 저장된다.

--- src/components/datasources/DataSource.tsx
+++ src/components/datasources/DataSource.tsx
@@ -5,40 +5,59 @@ import { Card, Row, Col } from 'antd';
 interface Source {
     name: string;
     img: string;
+    disabled: boolean;
 }
 
 interface Props {
     datasources: Array<Source>;
-    url: string;
+    onGetDatasource: () => string | null;
+    onSetDatasource: (datasource: string) => void;
+    onRemoveDatasource: () => void;
 }
 
-const DataSource: React.FunctionComponent<Props> = ({ children, datasources, url }) => {
-    const [selectedKey, setSelectedKey] = useState('');
-    const [active, setActive] = useState(false);
-    const onSelect = (event: any, name: string) => {
-        setSelectedKey(name);
+const DataSource: React.FunctionComponent<Props> = ({
+    datasources,
+    onSetDatasource,
+    onGetDatasource,
+    onRemoveDatasource,
+}) => {
+    const initSelectedKey = onGetDatasource();
+    const [selectedKey, setSelectedKey] = useState(initSelectedKey);
+
+    const onSelect = (datasource: Source) => {
+        if (datasource.disabled) {
+            return;
+        }
+        if (selectedKey !== datasource.name) {
+            setSelectedKey(datasource.name);
+            onSetDatasource(datasource.name);
+        } else {
+            setSelectedKey('');
+            onRemoveDatasource();
+        }
     };
     return (
         <div className={styles.datasource}>
-            <h2>Selected Data Source {selectedKey && `[ ${selectedKey} ]`}</h2>
+            <h2>Selected Data Source [ {selectedKey && `${selectedKey}`} ]</h2>
             <Row gutter={16}>
                 {datasources.length > 0 &&
                     datasources.map(datasource => {
                         return (
-                            <span key={datasource.name} onClick={e => onSelect(e, datasource.name)}>
-                                <Col span={6}>
-                                    <Card
-                                        title={
-                                            <img className={styles.img} src={datasource.img} alt={datasource.name} />
-                                        }
-                                        hoverable={true}
-                                        className={styles.card}
-                                        bordered={true}
-                                    >
-                                        <b>{datasource.name}</b>
-                                    </Card>
-                                </Col>
-                            </span>
+                            <Col
+                                onClick={() => onSelect(datasource)}
+                                className={datasource.name === selectedKey ? styles.selectedCol : styles.col}
+                                span={6}
+                                key={datasource.name}
+                            >
+                                <Card
+                                    title={<img className={styles.img} src={datasource.img} alt={datasource.name} />}
+                                    hoverable={true}
+                                    className={datasource.disabled ? styles.disabledCard : styles.card}
+                                >
+                                    <b>{datasource.name}</b>
+                                    <p style={{ fontStyle: 'italic' }}>{datasource.disabled && 'comming soon'}</p>
+                                </Card>
+                            </Col>
                         );
                     })}
             </Row>

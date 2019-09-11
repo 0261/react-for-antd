8c53b0aa865fc33d49690fe6e94fd2ddc06b2fd9
0261 <bada013@naver.com>
2019-09-11 11:21:36 +0900

    feature: 코드 스프릿팅 적용

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
 import importedComponent from 'react-imported-component';
 const AsyncDashboard = importedComponent(() =>
     import(/* webpackChunkName:'Dashboard' */ '../containers/dashboards/Dashboard'),
@@ -8,14 +7,18 @@ const AsyncDashboard = importedComponent(() =>
 const AsyncDataSource = importedComponent(() =>
     import(/* webpackChunkName:'Datasource' */ '../containers/datasources/DataSource'),
 );
-const AsyncHome = importedComponent(() => import(/* webpackChunkName:'Datasource' */ '../containers/home/Home'));
+const AsyncHome = importedComponent(() => import(/* webpackChunkName:'Home' */ '../containers/home/Home'));
+const AsyncConfigure = importedComponent(() =>
+    import(/* webpackChunkName:'SettingConfigure' */ '../containers/settings/configure/Configure'),
+);
 
 const App = () => {
     return (
         <Switch>
             <Route exact path='/' component={AsyncHome} />
-            <Route exact path='/datasource' component={AsyncDataSource} />
-            <Route exact path='/dashboard' component={AsyncDashboard} />
+            <Route path='/datasource' component={AsyncDataSource} />
+            <Route path='/dashboard' component={AsyncDashboard} />
+            <Route path='/setting/configure' component={AsyncConfigure} />
         </Switch>
     );
 };

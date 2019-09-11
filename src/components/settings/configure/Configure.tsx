0f99f81eda39e74fd708d209c5132081d2ed91ef
김바다 / Pada <bada013@naver.com>
2019-09-11 11:16:35 +0900

    데이터소스 관련 설정이 가능한 설정 컴포넌트 추가 (#19)
    * feature: 설정 컨테이너 추가
    
    * feature: 설정 컴포넌트 추가
    
    * feature: 설정 css추가
    
    * feature: 설정 컴포넌트 라우터 추가

---
+++ src/components/settings/configure/Configure.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './Configure.scss';
+
+const Configure: React.FunctionComponent = children => {
+    return <div className={styles.configure}>This is Configure</div>;
+};
+export default Configure;

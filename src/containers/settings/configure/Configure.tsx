0f27b6b45dfdc7f4c78c2e1fcd6a8e42c2656ea2
0261 <bada013@naver.com>
2019-09-11 15:23:50 +0900

    fix: 안쓰는 파일 삭제

--- src/containers/settings/configure/Configure.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import ConfigureComponent from '../../../components/settings/configure/Configure';
-class ConfigureContainers extends Component {
-    render() {
-        return <ConfigureComponent></ConfigureComponent>;
-    }
-}
-
-export default ConfigureContainers;

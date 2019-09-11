0b7978ef99e12a5da790a95472350113ae6deb43
0261 <bada013@naver.com>
2019-09-11 11:08:08 +0900

    feature: 브레드크럼 적용
    설정파일 값과 리액트 라우터 로케이션 값을 이용해 동적으로 현재 위치를 그려준다.

--- src/components/base/Header/Header.tsx
+++ src/components/base/Header/Header.tsx
@@ -1,21 +1,51 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Layout from 'antd/lib/layout';
 import Menu from 'antd/lib/menu';
 import Icon from 'antd/lib/icon';
 import styles from './Header.scss';
+import { withRouter, RouteComponentProps } from 'react-router';
+import { Breadcrumb } from 'antd';
+import { Link } from 'react-router-dom';
+import config from '../../../../config';
 const { Header } = Layout;
 
-const HeaderFC: React.FunctionComponent = () => {
+interface Props extends RouteComponentProps {}
+interface BreadcrumbName {
+    [key: string]: string;
+}
+const HeaderComponent: React.FunctionComponent<Props> = ({ children, location }) => {
+    const pathSnippets = location.pathname.split('/').filter(pathname => pathname);
+    const extraBreadcrumbItems = pathSnippets.map((_, index) => {
+        const url = `/${pathSnippets.slice(0, index + 1).join('/')}`;
+        return (
+            <Breadcrumb.Item key={url}>
+                {url !== '/setting' && <Link to={url}>{config.breadcrumbNameMap[url]}</Link>}
+            </Breadcrumb.Item>
+        );
+    });
+    const initBreadCrumbItems = [
+        <Breadcrumb.Item key='home'>
+            <Link to='/'>Home</Link>
+        </Breadcrumb.Item>,
+    ].concat(extraBreadcrumbItems);
+
     return (
         <Header className={styles.header}>
-            <Menu theme='light' mode='horizontal' selectable className={styles.menu}>
-                <Menu.Item key='github'>
-                    <a href='https://github.com/0261/react-for-antd'>
-                        <Icon type='github' className={styles.icon}></Icon>
-                    </a>
-                </Menu.Item>
-            </Menu>
+            <div className={styles.align}>
+                <Menu theme='light' mode='horizontal' selectable className={styles.menu}>
+                    <Menu.Item className={styles.menuItem} key='bread'>
+                        <Breadcrumb>{initBreadCrumbItems}</Breadcrumb>
+                    </Menu.Item>
+                </Menu>
+                <Menu theme='light' mode='horizontal' selectable className={styles.menu}>
+                    <Menu.Item className={styles.menuItem} key='github' style={{}}>
+                        <a href='https://github.com/0261/react-for-antd'>
+                            <Icon className={styles.icon} style={{ marginRight: 0 }} type='github'></Icon>
+                        </a>
+                    </Menu.Item>
+                </Menu>
+            </div>
         </Header>
     );
 };
-export default HeaderFC;
+export default withRouter(HeaderComponent);

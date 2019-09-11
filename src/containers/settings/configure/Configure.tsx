16a977a7f769a1bc3d8c071c292d7b58c8a5a5ef
0261 <bada013@naver.com>
2019-09-11 11:14:41 +0900

    feature: 설정 컨테이너 추가

---
+++ src/containers/settings/configure/Configure.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import ConfigureComponent from '../../../components/settings/configure/Configure';
+class ConfigureContainers extends Component {
+    render() {
+        return <ConfigureComponent></ConfigureComponent>;
+    }
+}
+
+export default ConfigureContainers;

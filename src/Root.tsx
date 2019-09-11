ceb70358af17224c68fe1a80824e4d142c770295
0261 <bada013@naver.com>
2019-09-11 11:21:25 +0900

    feature: Footer 주석처리
    footer를 쓸 일이 없다.

--- src/Root.tsx
+++ src/Root.tsx
@@ -17,7 +17,7 @@ const Root: React.FunctionComponent = () => {
                     <Content>
                         <App />
                     </Content>
-                    <Footer />
+                    {/* <Footer /> */}
                 </Layout>
             </Layout>
         </BrowserRouter>

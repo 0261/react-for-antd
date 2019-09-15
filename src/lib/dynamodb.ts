2959462b7e38040a4c0d36d4d020bbe353d22bde
김바다 / Pada <bada013@naver.com>
2019-09-15 15:49:29 +0900

    feature: 다이나모디비 테이블 리스트 가져오는 함수 추가 (#31)

--- src/lib/dynamodb.ts
+++ src/lib/dynamodb.ts
@@ -1,16 +1,20 @@
-import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+import AWS from 'aws-sdk';
 import { Expression } from '@typemon/dynamodb-expression';
-
 const accessKeyId = localStorage.getItem('accesskey') || '';
 const secretAccessKey = localStorage.getItem('secretkey') || '';
-console.log(accessKeyId, secretAccessKey);
 
+console.log(accessKeyId, secretAccessKey);
 interface Option {
     Limit?: number;
     ScanIndexForward?: boolean;
     ExclusiveStartKey?: { [key: string]: any };
 }
-const client = new DocumentClient({
+const client = new AWS.DynamoDB.DocumentClient({
+    region: 'ap-northeast-2',
+    accessKeyId,
+    secretAccessKey,
+});
+const dynamodb = new AWS.DynamoDB({
     region: 'ap-northeast-2',
     accessKeyId,
     secretAccessKey,
@@ -24,7 +28,7 @@ export const query = async (
     index?: string,
 ): Promise<Array<any>> => {
     try {
-        const parameter: DocumentClient.QueryInput = {
+        const parameter: AWS.DynamoDB.DocumentClient.QueryInput = {
             TableName: tableName,
             KeyConditionExpression: keyConditionExpression.expression,
             ExpressionAttributeNames: Object.assign(
@@ -39,11 +43,20 @@ export const query = async (
             IndexName: index ? index : undefined,
             ...option,
         };
-        console.log(parameter);
         const result = await client.query(parameter).promise();
         return result.Items as any;
     } catch (error) {
         console.log('query', error);
+        throw new Error(error);
+    }
+};
+
+export const listTables = async () => {
+    try {
+        const tables = await dynamodb.listTables().promise();
+        return tables;
+    } catch (error) {
+        console.log('list tables', error);
         throw new Error(error);
     }
 };

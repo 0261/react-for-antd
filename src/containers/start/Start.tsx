c54f57df06dc73bc5eba816d4cbdbec0d30845d4
0261 <bada013@naver.com>
2019-09-16 17:54:18 +0900

    feature: 차트 추가

--- src/containers/start/Start.tsx
+++ src/containers/start/Start.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import StartComponent from '../../components/start/Start';
-import { listTables } from '../../lib/dynamodb';
+import { listTables, getTableDescription } from '../../lib/dynamodb';
 
 const steps = [
     {
@@ -26,27 +26,27 @@ const steps = [
 ];
 const dataSources = [
     {
-        name: 'DYNAMODB',
+        name: 'Dynamodb',
         img: '/src/static/img/dynamodb.svg',
         disabled: false,
     },
     {
-        name: 'MYSQL',
+        name: 'Mysql',
         img: '/src/static/img/mysql.svg',
         disabled: true,
     },
     {
-        name: 'FIREBASE',
+        name: 'Firebase',
         img: '/src/static/img/firebase.svg',
         disabled: true,
     },
     {
-        name: 'MONGO',
+        name: 'Mongo',
         img: '/src/static/img/mongo.svg',
         disabled: true,
     },
     {
-        name: 'REIDS',
+        name: 'Redis',
         img: '/src/static/img/redis.svg',
         disabled: true,
     },
@@ -56,11 +56,37 @@ const dataSources = [
         disabled: true,
     },
 ];
+const charts = [
+    {
+        displayName: 'Pie Chart',
+        name: 'Pie',
+        disabled: false,
+        img: '/src/static/img/pie.png',
+    },
+    {
+        displayName: 'Bar Chart',
+        name: 'Bar',
+        disabled: true,
+        img: '/src/static/img/bar.png',
+    },
+    {
+        displayName: 'Network Chart',
+        name: 'Network',
+        disabled: true,
+        img: '/src/static/img/network.png',
+    },
+];
+interface TableDescription {
+    AttributeName: string;
+    KeyType: 'HASH' | 'RANGE' | string;
+}
 interface State {
     current: number;
     tables: Array<string>;
     dataSource: string;
     table: string;
+    chart: string;
+    tableDescription: Array<TableDescription>;
 }
 export default class Start extends Component<{}, State, {}> {
     constructor(props: any) {
@@ -70,6 +96,8 @@ export default class Start extends Component<{}, State, {}> {
             tables: [],
             dataSource: this.handlGetDatasource(),
             table: this.handleGetTable(),
+            chart: this.handleGetChart(),
+            tableDescription: [],
         };
     }
     nextCurrent = () => {
@@ -95,7 +123,7 @@ export default class Start extends Component<{}, State, {}> {
     };
 
     handleGetTableNames = async (dataSource: string | null) => {
-        if (dataSource === 'DYNAMODB') {
+        if (dataSource === 'Dynamodb') {
             const tables = await listTables();
             this.setState({ current: this.state.current, tables: tables.TableNames || [] });
         }
@@ -112,7 +140,31 @@ export default class Start extends Component<{}, State, {}> {
 
     handleRemoveTable = () => {
         localStorage.removeItem('table');
-        this.setState({ ...this.state, table: '' });
+        this.setState({ ...this.state, table: '', tableDescription: [] });
+    };
+
+    handleGetTableDescription = async (dataSource: string | null, table: string) => {
+        try {
+            if (dataSource === 'Dynamodb') {
+                const { KeySchema, tableName } = await getTableDescription(table);
+                this.setState({
+                    ...this.state,
+                    tableDescription: KeySchema,
+                });
+            }
+        } catch (error) {}
+    };
+
+    handleGetChart = () => {
+        return localStorage.getItem('chart') || '';
+    };
+    handleSetChart = (chart: string) => {
+        localStorage.setItem('chart', chart);
+        this.setState({ ...this.state, chart });
+    };
+    handleRemoveChart = () => {
+        localStorage.removeItem('chart');
+        this.setState({ ...this.state, chart: '' });
     };
 
     render() {
@@ -121,8 +173,11 @@ export default class Start extends Component<{}, State, {}> {
                 steps={steps}
                 dataSources={dataSources}
                 dataSource={this.state.dataSource}
+                charts={charts}
+                chart={this.state.chart}
                 tables={this.state.tables}
                 table={this.state.table}
+                tableDescription={this.state.tableDescription}
                 onNextCurrent={this.nextCurrent}
                 onPrevCurrent={this.prevCurrent}
                 current={this.state.current}
@@ -133,6 +188,10 @@ export default class Start extends Component<{}, State, {}> {
                 onGetTable={this.handleGetTable}
                 onSetTable={this.handleSetTable}
                 onRemoveTable={this.handleRemoveTable}
+                onGetTableDescription={this.handleGetTableDescription}
+                onGetChart={this.handleGetChart}
+                onSetChart={this.handleSetChart}
+                onRemoveChart={this.handleRemoveChart}
             ></StartComponent>
         );
     }

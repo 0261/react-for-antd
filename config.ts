0971673fb240be06640b9451b36cc19a7f09e605
0261 <bada013@naver.com>
2019-09-16 17:51:25 +0900

    fix: 안쓰는 브레드크럼브 삭제

--- config.ts
+++ config.ts
@@ -9,9 +9,6 @@ export default {
         '/setting': 'Settings',
         '/chart': 'Charts',
         '/setting/aws': 'AWS',
-        '/chart/pie': 'Pie',
-        '/chart/bar': 'Bar',
-        '/chart/network': 'Network',
         // '/setting/firebase': 'Firebase',
     } as BreadcrumbName,
 };

32b4126cd8433c7754766f4c28426a53583bd73e
0261 <bada013@naver.com>
2019-09-22 15:18:25 +0900

    fix: double quote -> single

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { rootReducer } from "./store";
+import { rootReducer } from './store';
 
 import Root from './Root';
 import './styles/app.css';

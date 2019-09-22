67874748b39b2570923b8e2c33a9a87028e1c662
0261 <bada013@naver.com>
2019-09-22 14:37:39 +0900

    build: 리덕스 개발툴 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom';
 import Root from './Root';
 import './styles/app.css';
 
+interface CustomWindow extends Window {
+    __REDUX_DEVTOOLS_EXTENSION__: any;
+}
+const customWindow = window as CustomWindow;
+const devTools = customWindow.__REDUX_DEVTOOLS_EXTENSION__ && customWindow.__REDUX_DEVTOOLS_EXTENSION__();
+
 ReactDOM.render(<Root />, document.getElementById('root'));

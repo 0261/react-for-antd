fa7517e0f70da2f737ea931bddca6e98360d989f
0261 <bada013@naver.com>
2019-09-24 23:11:24 +0900

    feature: 대시보드 레이아웃 추가

--- src/components/dasboards/Dashboard.tsx
+++ src/components/dasboards/Dashboard.tsx
@@ -1,6 +1,7 @@
 // import React, { useState } from 'react';
 // import styles from './Dashboard.scss';
 // import { Sankey } from '@nivo/sankey';
+import SankeyChart from '../charts/sankey/Sankey';
 // import { Resizable, ResizableBox } from 'react-resizable';
 // import { contains } from '@typemon/dynamodb-expression';
 
@@ -11,7 +12,91 @@
 // };
 
 // export default Dashboard;
-
+const data = {
+    nodes: [
+        {
+            id: 'John',
+            color: 'hsl(206, 70%, 50%)',
+        },
+        {
+            id: 'Raoul',
+            color: 'hsl(191, 70%, 50%)',
+        },
+        {
+            id: 'Jane',
+            color: 'hsl(26, 70%, 50%)',
+        },
+        {
+            id: 'Marcel',
+            color: 'hsl(168, 70%, 50%)',
+        },
+        {
+            id: 'Ibrahim',
+            color: 'hsl(310, 70%, 50%)',
+        },
+        {
+            id: 'Junko',
+            color: 'hsl(216, 70%, 50%)',
+        },
+    ],
+    links: [
+        {
+            source: 'Jane',
+            target: 'Junko',
+            value: 194,
+        },
+        {
+            source: 'Jane',
+            target: 'Marcel',
+            value: 73,
+        },
+        {
+            source: 'Jane',
+            target: 'Ibrahim',
+            value: 141,
+        },
+        {
+            source: 'Jane',
+            target: 'John',
+            value: 199,
+        },
+        {
+            source: 'Jane',
+            target: 'Raoul',
+            value: 39,
+        },
+        {
+            source: 'Raoul',
+            target: 'Junko',
+            value: 11,
+        },
+        {
+            source: 'Raoul',
+            target: 'John',
+            value: 54,
+        },
+        {
+            source: 'Marcel',
+            target: 'John',
+            value: 83,
+        },
+        {
+            source: 'Marcel',
+            target: 'Raoul',
+            value: 193,
+        },
+        {
+            source: 'Junko',
+            target: 'John',
+            value: 98,
+        },
+        {
+            source: 'John',
+            target: 'Ibrahim',
+            value: 108,
+        },
+    ],
+};
 import React from 'react';
 import RGL, { WidthProvider } from 'react-grid-layout';
 
@@ -57,14 +142,13 @@ class MessyLayout extends React.PureComponent<Props, State> {
     }
 
     generateLayout() {
-        const p = this.props;
         return [1, 2, 3, 4, 5, 67, 8, 9, 10, 12, 32, 36456, 456, 457].map(function(i) {
             const w = Math.ceil(Math.random() * 4);
             const y = Math.ceil(Math.random() * 4) + 1;
             return {
                 x: (i * 2) % 12,
                 y: Math.floor(i / 6) * y,
-                w: 300,
+                w: w,
                 h: y,
                 i: i.toString(),
             };
